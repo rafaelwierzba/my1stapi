889d86807431a04e6fb82113ed8e60989800637c
Rafa Wierzba <rafaelwierzba@gmail.com>
2023-01-20 15:06:31 -0300

    Add files via upload

---
+++ BookAPI/Repositories/BookRepository.cs
@@ -0,0 +1,46 @@
+﻿using BookAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        public readonly BookContext _context;
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+        }
+        public async Task<Book> Create(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<Book> Delete(int Id)
+        {
+            var bookToDelete = await _context.Books.FindAsync(Id);
+            _context.Books.Remove(bookToDelete);
+            await _context.SaveChangesAsync();
+            return null;
+
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+          return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book> Get(int Id)
+        {
+            return await _context.Books.FindAsync(Id);
+        }
+
+        public async Task<Book> Update(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+             await _context.SaveChangesAsync();
+            return book;
+        }
+    }
+}
---
+++ BookAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookAPI.Model;
+
+namespace BookAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int Id);
+        Task<Book> Create(Book book);
+        Task<Book> Update(Book book);
+        Task<Book> Delete(int Id);
+    }
+}
